fcadefc88fbe0570c371d549694017e3ec7a9070
AndreRomero2003 <aromerom3@est.ups.edu.ec>
2026-01-22 21:09:51 -0500

    Update medical_processor_gui.py

--- medical_processor_gui.py
+++ medical_processor_gui.py
@@ -44,11 +44,11 @@ class MedicalImageProcessor4Core(ctk.CTk):
         # Grids optimizados para sistemas de 4 núcleos
         self.clip_grid = [1, 2, 3, 4, 6, 8, 12, 16]  # CLAHE clipLimit
         self.tile_grid = [(4,4), (6,6), (8,8), (12,12), (16,16), (24,24), (32,32)]  # CLAHE tileGridSize
-        self.radius_grid = [2, 3, 4, 6, 8, 12, 16, 24, 32]  # Filtro Guiado radius
+        self.radius_grid = [1, 3, 4, 6, 8, 12, 16, 24, 32]  # Filtro Guiado radius
         self.eps_grid = [1e-6, 3e-6, 1e-5, 3e-5, 1e-4, 3e-4, 1e-3, 3e-3, 1e-2]  # Filtro Guiado epsilon
-        self.amount_grid = [0.1, 0.2, 0.3, 0.6, 1.0, 1.6, 2.0, 2.2]  # Unsharp Mask amount
+        self.amount_grid = [0.0, 0.2, 0.3, 0.6, 1.0, 1.6, 2.0, 2.2]  # Unsharp Mask amount
         self.sigma_grid = [0.5, 0.8, 1.2, 1.8, 2.5, 3.0, 3.5]  # Unsharp Mask sigma
-        self.threshold_grid = [0.0, 0.005, 0.01, 0.02, 0.05, 0.1]  # Unsharp Mask threshold
+        self.threshold_grid = [0.1, 0.005, 0.01, 0.02, 0.05, 0.1]  # Unsharp Mask threshold
         
         # Parámetros iniciales
         self.clahe_clip_limit = self.clip_grid[1]  # 2
@@ -683,3 +683,4 @@ if __name__ == "__main__":
     
     app = MedicalImageProcessor4Core()
     app.mainloop()
+
